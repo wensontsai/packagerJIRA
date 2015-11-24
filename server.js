318266e4f21dae7fdfdfb212a80f98d80279e5b8
Wenson Tsai <wensontsai@gmail.com>
2015-11-24 15:29:56 -0500

    idk maannnn

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var port = 8000;
 
 var Client = require('node-rest-client').Client;
@@ -12,7 +14,7 @@ var Client = require('node-rest-client').Client;
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cookieParser());
 
 // enabling CORS //
 app.use(function(req, res, next) {
@@ -44,48 +46,51 @@ app.use('/api', apiRoutes);
 
 
 apiRoutes.post('/authJira', function(req, res, next){
-	console.log('yo wtf');
 	client = new Client();
 	// Provide user credentials, which will be used to log in to JIRA.
 	var loginArgs = {
-	        data: {
-	                "username": req.body.username,
-	                "password": req.body.password
-	        },
-	        headers: {
-	                "Content-Type": "application/json"
-	        } 
+        data: {
+            "username": req.body.username,
+            "password": req.body.password
+        },
+        headers: {
+            "Content-Type": "application/json"
+        } 
 	};
-console.log(loginArgs.data);
 	client.post("https://dressler.atlassian.net/rest/auth/1/session", loginArgs, function(data, response){
-	        if (response.statusCode == 200) {
-	                console.log('succesfully logged in, session:', data.session);
-	                var session = data.session;
-	                // Get the session information and store it in a cookie in the header
-	                var searchArgs = {
-	                        headers: {
-									// Set the cookie from the session information
-	                                cookie: session.name + '=' + session.value,
-	                                "Content-Type": "application/json"
-	                        },
-	                        data: {
-									// Provide additional data for the JIRA search. You can modify the JQL to search for whatever you want.
-	                                jql: "type=Bug AND status=Closed"
-	                        }
-	                };
-					// Make the request return the search results, passing the header information including the cookie.
-	                client.post("https://dressler.atlassian.net/rest/api/2/search", searchArgs, function(searchResult, response) {
-	                        console.log('status code:', response.statusCode);
-	                        console.log('search result:', searchResult);
-	                });
-	        }
-	        else {
-	                console.log("Login failed :(");
-	        }
+        if (response.statusCode == 200) {
+            console.log('succesfully logged in, session:', data.session);
+            var session = data.session;
+			var token = session.name + '=' + session.value;
+			// set cookie //
+			res.cookie(session.name, session.value, { maxAge: 900000, httpOnly: false}).send('Token is set as cookie');
+        }
+        else {
+                console.log("Login failed :(");
+        }
 	});
 });
 
+apiRoutes.post('/getIssue', function(req, res, next){
+	// Get the session information and store it in a cookie in the header
+	var searchArgs = {
+        headers: {
+			// Set the cookie from the session information
+            cookie: session.name + '=' + session.value,
+            "Content-Type": "application/json"
+        },
+        data: {
+			// Provide additional data for the JIRA search. You can modify the JQL to search for whatever you want.
+            // jql: "type=Bug AND status=Closed"
+        }
+	};
 
+	// Make the request return the search results, passing the header information including the cookie.
+	client.post("https://dressler.atlassian.net/rest/api/latest/issue/WQS-14", searchArgs, function(searchResult, response) {
+	    console.log('status code:', response.statusCode);
+	    console.log('search result:', searchResult);
+	});
+});
 
 
 
