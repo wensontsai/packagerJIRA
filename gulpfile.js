e8a2d89761710cbc2249349ff2051d061160474c
Wenson Tsai <wensontsai@gmail.com>
2015-11-25 13:00:20 -0500

    gulpfile working with nodemon livereload solid baby

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var util = require('gulp-util');
 var nodemon = require('gulp-nodemon');
+var livereload = require('gulp-livereload');
 
 
 var notify = function(error) {
@@ -59,21 +60,38 @@ gulp.task('build', function() {
   bundle()
 });
 
-gulp.task('serve', function(done) {
-  gulp.src('')
-    .pipe(server({
-      livereload: {
-        enable: true,
-        filter: function(filePath, cb) {
-          if(/main.js/.test(filePath)) {
-            cb(true)
-          } else if(/style.css/.test(filePath)){
-            cb(true)
-          }
-        }
-      },
-      open: false
-    }));
+// gulp.task('serve', function(done) {
+//   gulp.src('')
+//     .pipe(server({
+//       livereload: {
+//         enable: false,
+//         filter: function(filePath, cb) {
+//           if(/main.js/.test(filePath)) {
+//             cb(true)
+//           } else if(/style.css/.test(filePath)){
+//             cb(true)
+//           }
+//         }
+//       },
+//       open: false
+//     }));
+// });
+
+gulp.task('serve', function (cb) {
+    nodemon({
+        script  : 'server.js',
+        watch   : ['js', 'html'],
+        //...add nodeArgs: ['--debug=5858'] to debug 
+        //..or nodeArgs: ['--debug-brk=5858'] to debug at server start
+    }).on('start', function () {
+        setTimeout(function () {
+            livereload.changed();
+        }, 2000); // wait for the server to finish loading before restarting the browsers
+    })
+    .on('change', ['watch'])
+    .on('restart', function () {
+      console.log('restarted!');
+    });
 });
 
 gulp.task('sass', function () {
@@ -83,27 +101,40 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./public/'));
 });
 
-// gulp.task('default', function() {
-//   // listen for changes
-//   livereload.listen();
-//   // configure nodemon
-//   nodemon({
-//     // the script to run the app
-//     script: 'server.js',
-//     ext: 'js'
-//   }).on('restart', function(){
-//     // when the app has restarted, run livereload.
-//     gulp.src('server.js')
-//       .pipe(livereload())
-//       .pipe(notify('Reloading page, please wait...'));
-//   })
-// })
+gulp.task('nodemon', function() {
+  // // listen for changes
+  // livereload.listen();
+  // // configure nodemon
+  // nodemon({
+  //   // the script to run the app
+  //   script: 'server.js',
+  //   ext: 'js'
+  // }).on('restart', function(){
+  //   // when the app has restarted, run livereload.
+  //   gulp.src('server.js')
+  //     .pipe(livereload())
+  //     .pipe(notify('Reloading page, please wait...'));
+  // })
+  nodemon({
+      script: 'server.js',
+      ext: 'js',
+      env: {
+        'NODE_ENV': 'development'
+      }
+    })
+      .on('start', ['watch'])
+      .on('change', ['watch'])
+      .on('restart', function () {
+        console.log('restarted!');
+      });
+})
 
 
 gulp.task('watch', function () {
+  livereload.listen();
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 // gulp.task('default', ['build', 'serve', 'sass', 'watch']);
-gulp.task('default', ['build','sass']);
+gulp.task('default', ['build', 'serve', 'sass', 'watch']);
 
